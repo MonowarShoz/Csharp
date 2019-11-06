a1247ba3724a9d9eeef954497626ce7b253fc714
MonowarShoz <shozolhorn@gmail.com>
2019-11-07 00:23:45 +0600

    exam

---
+++ prob3/prob3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace prob3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diag1=0, diag2=0;
+            int[,] arr = new int[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+
+                for (int j = 0; j < 4; j++)
+                {
+                    arr[i, j] = Int32.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                     
+                    if (i == j)
+                        diag1 += arr[i, j];
+
+                    
+                    if (i == 4 - j - 1)
+                        diag2 += arr[i, j];
+                }
+            }
+            Console.WriteLine("answer is: "+ (diag1+diag2));
+        }
+    }
+}
